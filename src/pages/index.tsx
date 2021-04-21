35e275d8382de31939460ea50dcc8258cf1c4200
Luiz Henrique <7henrique18@gmail.com>
2021-04-21 13:45:19 -0300

    executando build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) { 
+  console.log(props.episodes)
+
+  /*
+  SPA - single page application 
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  })
+  */
+
   return (
-    <h1>index</h1>
+    <>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
   )
+}
+
+//SSR - server side rendering
+/*
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data
+    }
+  }
+}
+*/
+
+// SSG - static side generation 
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
