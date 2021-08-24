75489bd0c1cce06ba88dd4845c7c300e8d75f32a
Luiz Henrique <7henrique18@gmail.com>
2021-08-24 11:21:10 -0300

    fix: db json

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,15 +1,15 @@
-import { format, parseISO } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
-import { GetStaticPaths, GetStaticProps } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
-import Head from 'next/head';
-import { api } from '../../services/api';
-import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
+import { api } from "../../services/api";
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
 
-import styles from './episode.module.scss'
-import { useContext } from 'react'
-import { PlayerContext, usePlayer } from '../../contexts/PlayerContext';
+import styles from "./episode.module.scss";
+import { useContext } from "react";
+import { PlayerContext, usePlayer } from "../../contexts/PlayerContext";
 
 type Episode = {
   id: string;
@@ -22,11 +22,11 @@ type Episode = {
   durationAsString: string;
   description: string;
   //..
-}
+};
 
 type EpisodeProps = {
   episode: Episode;
-}
+};
 
 export default function Episode({ episode }: EpisodeProps) {
   const { play } = usePlayer();
@@ -46,11 +46,16 @@ export default function Episode({ episode }: EpisodeProps) {
 
         <Image
           width={700}
-          height={160} src={episode.thumbnail}
+          height={160}
+          src={episode.thumbnail}
           objectFit="cover"
         />
         <button type="button">
-          <img src="/play.svg" alt="Tocar episódio" onClick={() => play(episode)} />
+          <img
+            src="/play.svg"
+            alt="Tocar episódio"
+            onClick={() => play(episode)}
+          />
         </button>
       </div>
 
@@ -66,54 +71,54 @@ export default function Episode({ episode }: EpisodeProps) {
         dangerouslySetInnerHTML={{ __html: episode.description }}
       />
     </div>
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data } = await api.get('episodes', {
+  const { data } = await api.get("episodes", {
     params: {
       _limit: 2,
-      _sort: 'published_at',
-      _order: 'desc'
-    }
-  })
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
-
-  const paths = data.map(episode => {
+  const paths = data.map((episode) => {
     return {
       params: {
-        slug: episode.id
-      }
-    }
-  })
+        slug: episode.id,
+      },
+    };
+  });
 
   return {
     paths,
-    fallback: 'blocking'
-  }
-}
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params;
-  const { data } = await api.get(`/episodes/${slug}`)
+  const { data } = await api.get(`/episodes/${slug}`);
 
   const episode = {
     id: data.id,
     title: data.title,
     thumbnail: data.thumbnail,
     members: data.members,
-    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    publishedAt: format(parseISO(data.published_at), "d MMM yy", {
+      locale: ptBR,
+    }),
     duration: Number(data.file.duration),
     durationAsString: convertDurationToTimeString(Number(data.file.duration)),
     url: data.file.url,
-    description: data.description
+    description: data.description,
   };
 
   return {
     props: {
       episode,
     },
-    revalidate: 60 * 60 * 24 // 24 hours (1 day)
-  }
-
-}
+    revalidate: 60 * 60 * 24, // 24 hours (1 day)
+  };
+};
